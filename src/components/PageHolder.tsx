cc03ee6c5eacb92977098f7837df5f5ba3928a5b
Swarnendu <swarnendumaity633@gmail.com>
2025-01-11 11:25:18 +0530

    fix: Fixes the UI Structure

--- src/components/PageHolder.tsx
+++ src/components/PageHolder.tsx
@@ -1,18 +1,17 @@
 function PageHolder() {
-    return (
-      <div className="flex pl-14 pt-24">
-        <div className="text-sm text-[#3E5765]">
-          Cryptocurrencies
-        </div>
-        <div className="text-sm text-[#3E5765]">
-          {' >> '}
-        </div>
-        <div className="text-sm text-[#0F1629]">
-          Bitcoin
-        </div>
+  return (
+    <div className="flex pl-14 pt-4">
+      <div className="text-sm text-[#3E5765]">
+        Cryptocurrencies
       </div>
-    )
-  }
-  
-  export default PageHolder;
-  
+      <div className="text-sm text-[#3E5765]">
+        {' >> '}
+      </div>
+      <div className="text-sm text-[#0F1629]">
+        Bitcoin
+      </div>
+    </div>
+  )
+}
+
+export default PageHolder;
